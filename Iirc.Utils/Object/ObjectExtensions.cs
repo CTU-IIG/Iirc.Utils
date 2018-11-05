5ce5c949ac584ac24658ea5556ca557c59a6b1a4
Istvan Modos <istvan.modos@cvut.cz>
2018-11-05 15:19:39 +0100

    Add Object.DeepCopy extension method

---
+++ Iirc.Utils/Object/ObjectExtensions.cs
@@ -0,0 +1,20 @@
+namespace Iirc.Utils.Object
+{
+    using System.IO;
+    using System.Runtime.Serialization.Formatters.Binary;
+    using System.Runtime.Serialization;
+    
+    public static class ObjectExtensions
+    {
+        public static T DeepClone<T>(this T a)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(stream, a);
+                stream.Position = 0;
+                return (T) formatter.Deserialize(stream);
+            }
+        }
+    }
+}
